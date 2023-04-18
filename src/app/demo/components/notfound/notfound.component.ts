d0562929a5491837711d31adaed6e211c5d6c925
Kiko <kikoguirado@gmail.com>
2023-04-18 17:12:16 +0200

    added some images to not found

--- src/app/demo/components/notfound/notfound.component.ts
+++ src/app/demo/components/notfound/notfound.component.ts
@@ -4,4 +4,12 @@ import { Component } from '@angular/core';
     selector: 'app-notfound',
     templateUrl: './notfound.component.html',
 })
-export class NotfoundComponent { }
+export class NotfoundComponent {
+    imageUrl: string;
+
+    constructor() {
+        const imageNumber = Math.floor(Math.random() * 6) + 1;
+        this.imageUrl = `assets/img/gatos/${imageNumber}.png`;
+        // this.imageUrl = `assets/img/logos/logo1.png`
+    }
+}
